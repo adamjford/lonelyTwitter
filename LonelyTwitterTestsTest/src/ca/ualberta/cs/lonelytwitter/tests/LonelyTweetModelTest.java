fa7ac6723022bc6dafa48c488abe3a7741dc66d2
Adam Ford <adam.ford@greatbigsolutions.com>
2015-01-27 17:35:30 -0700

    Add LonelyTweetModelTest

---
+++ LonelyTwitterTestsTest/src/ca/ualberta/cs/lonelytwitter/tests/LonelyTweetModelTest.java
@@ -0,0 +1,14 @@
+package ca.ualberta.cs.lonelytwitter.tests;
+
+import ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class LonelyTweetModelTest extends ActivityInstrumentationTestCase2<LonelyTwitterActivity> {
+	public LonelyTweetModelTest() {
+		super(LonelyTwitterActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+}
